d6a9907a235622c5dee67a6beb3f9ff0345ceb92
Nino Dafonte <nino@dafonte.dev>
2023-12-01 18:10:45 +0100

    Set up aoc in swift, omg...

---
+++ aoc-2023/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  aoc-2023
+//
+//  Created by Nino Dafonte on 1/12/23.
+//
+
+import Foundation
+import aoc_2023_Framework
+
+print(isPalindrome("Hello, World!"))
+
+
---
+++ aoc_2023_Framework/aoc_2023_Framework.h
@@ -0,0 +1,18 @@
+//
+//  aoc_2023_Framework.h
+//  aoc_2023_Framework
+//
+//  Created by Nino Dafonte on 1/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for aoc_2023_Framework.
+FOUNDATION_EXPORT double aoc_2023_FrameworkVersionNumber;
+
+//! Project version string for aoc_2023_Framework.
+FOUNDATION_EXPORT const unsigned char aoc_2023_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aoc_2023_Framework/PublicHeader.h>
+
+
